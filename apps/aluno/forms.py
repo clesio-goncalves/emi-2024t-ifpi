623f45ad79562590844887474182af65b16bef48
clesio-goncalves <clesio.pi@hotmail.com>
2025-12-12 13:48:09 -0300

    forms.py

--- apps/aluno/forms.py
+++ apps/aluno/forms.py
@@ -2,4 +2,20 @@ from django import forms
 from apps.aluno.models import Aluno
 
 class AlunoForms(forms.ModelForm):
-    pass
+    class Meta:
+        model = Aluno
+        exclude = ["",]
+        labels = {
+            "matricula" : "Matrícula",
+            "status" : "Ativo"
+        }
+
+        widgets = {
+            "nome": forms.TextInput(attrs={"class": "form-control", 
+                                            "autofocus": "autofocus"}),
+            
+            "matricula": forms.TextInput(attrs={"class": "form-control"}),
+            "email": forms.EmailInput(attrs={"class": "form-control"}),
+            "telefone": forms.TextInput(attrs={"class": "form-control"}),
+            "status": forms.CheckboxInput(attrs={"class": "form-control form-check-input"})
+        }
